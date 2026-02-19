32a37e247e4db4013bc5347515163dd37fb62788
Rupakkundu99 <mailupsana@gmail.com>
2026-02-20 00:56:32 +0530

    Updated footer layout and hidden on specific pages

--- src/app/Events/layout.js
+++ src/app/Events/layout.js
@@ -1,13 +1,20 @@
 import { UserRegistrationPaymentProvider } from "@/context/RegistrationPaymentContext";
 import React from "react";
+// 1. Import the new footer
+import EventBlogFooter from "@/components/Utility/EventBlogFooter"; 
 
 const Eventlayout = ({ children }) => {
   return (
-    <div className="container sm:py-5 px-5 mt-10 md:mt-0  m-auto">
-      <UserRegistrationPaymentProvider>
-        {children}
-      </UserRegistrationPaymentProvider>
-    </div>
+    <> {/* Wrap in a fragment */}
+      <div className="container py-5 px-5 mt-20 md:mt-0 m-auto min-h-[60vh]">
+        <UserRegistrationPaymentProvider>
+          {children}
+        </UserRegistrationPaymentProvider>
+      </div>
+      
+      {/* 2. Add the footer at the bottom */}
+      <EventBlogFooter />
+    </>
   );
 };
 
