54f7d928f31433ca8ba74baeebd2d7eed75aae59
priyankab2701 <priyankaanil2701@gmail.com>
2026-03-09 00:07:55 +0530

    sponsor logo added

--- src/app/hackon/page.js
+++ src/app/hackon/page.js
@@ -1,12 +1,12 @@
+import FAQ from "@/components/Hackthon/FAQ.js";
 import FollowForUpdates from "@/components/Hackthon/FollowForUpdates";
 import HackOnInfo from "@/components/Hackthon/HackOnInfo";
-import HackOnPartners from "@/components/Hackthon/HackOnPartners";
-import FAQ from "@/components/Hackthon/FAQ.js";
 import HackOnPrizes from "@/components/Hackthon/HackOnPrizes";
 import JoinCommunity from "@/components/Hackthon/JoinCommunity";
 import LandingPageHackOn from "@/components/Hackthon/LandingPageHackOn";
 import WhyParticipant from "@/components/Hackthon/WhyParticipant";
 import FooterHackON from "@/components/Utility/FooterHackON";
+import HackOnPartners from "@/components/Hackthon/HackOnPartners";
 import Image from "next/image";
 
 export const metadata = {
@@ -18,16 +18,19 @@ export const metadata = {
 
 const Hackon = () => {
   return (
-    <div className="fixed  top-0  flex-col  flex md:gap-5 left-0 w-full  overflow-y-scroll overflow-scroll h-screen z-50 bg-Dark ">
+    <div className="fixed  top-0  flex-col  flex md:gap-5 left-0 w-full  overflow-y-auto overflow-x-hidden h-screen z-50 bg-black ">
       <LandingPageHackOn />
       <HackOnInfo />
+      {/* <HackOnPrizes /> */}
+      
+      <HackOnPrizes/>
       <WhyParticipant />
       <JoinCommunity />
       <FollowForUpdates />
-      <HackOnPrizes />
-      <HackOnPartners />
+      
+      { <HackOnPartners /> }
       <FAQ />
-      <Image src="/HackOnStrip.svg" width={20} height={30} className="w-full" />
+      {<Image src="/HackOnStrip.svg" width={20} height={30} className="w-full" /> }
       <FooterHackON />{" "}
     </div>
   );
